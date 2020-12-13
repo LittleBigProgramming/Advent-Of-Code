6f69cb16ff420e5f03cf1a25cbc84027dd5c5179
LittleBigProgramming <20643587+LittleBigProgramming@users.noreply.github.com>
2020-12-13 23:37:48 +0000

    Update day-5.php

--- day-5/day-5.php
+++ day-5/day-5.php
@@ -6,7 +6,7 @@ $seatCodes = explode(PHP_EOL, file_get_contents('input.txt'));
  * @param $seat
  * @return float|int
  */
-function convertToBinary($seat)
+function convertToBinaryThenDecimal($seat)
 {
     return bindec(preg_replace(['/[FL]/', '/[BR]/'], [0,1], $seat));
 }
@@ -19,15 +19,35 @@ function getfindMaxSeatId($seatCodes)
 {
     $binarySeatCodes = [];
     foreach($seatCodes as $i => $seat) {
-        array_push($binarySeatCodes, convertToBinary($seat));
+        array_push($binarySeatCodes, convertToBinaryThenDecimal($seat));
     }
 
     return max($binarySeatCodes);
 }
 
+/**
+ * @param $seatCodes
+ * @return mixed
+ */
+function getMySeatId($seats)
+{
+    return current(array_diff(range(min($seats), max($seats)), $seats));
+}
+
+/**
+ * @param $seatCodes
+ * @return array|float[]|int[]
+ */
+function mapSeatsFromCodes($seatCodes)
+{
+    return array_map(fn ($seat) => convertToBinaryThenDecimal($seat), $seatCodes);
+}
+
 $time_start = microtime(true);
 
-echo getfindMaxSeatId($seatCodes) . PHP_EOL;
+echo 'Seat ID: ' . getfindMaxSeatId($seatCodes) . PHP_EOL;
+$seats = mapSeatsFromCodes($seatCodes);
+echo 'My Seat ID: ' . getMySeatId($seats) . PHP_EOL;;
 
 $time_end = microtime(true);
 $execution_time = ($time_end - $time_start);
