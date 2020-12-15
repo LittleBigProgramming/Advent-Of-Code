436e8d06970ade1f3b57af9d963ee835dc91bf99
LittleBigProgramming <20643587+LittleBigProgramming@users.noreply.github.com>
2020-12-15 23:17:41 +0000

    Part 2

--- day-6/day-6.php
+++ day-6/day-6.php
@@ -6,7 +6,7 @@ $answers = explode("\n\n", file_get_contents('input.txt'));
  * @param $answers
  * @return int
  */
-function countUniqueAnswers($answers)
+function countUniqueAnswers($answers): int
 {
     $count = 0;
 
@@ -21,18 +21,42 @@ function countUniqueAnswers($answers)
 }
 
 /**
+ * @param $answers
+ * @return int
+ */
+function countCommonAnswers($answers): int
+{
+    $count = 0;
+
+    foreach ($answers as $group) {
+
+        $groupAnswers = array_map('str_split', explode(PHP_EOL, $group));
+
+        if (count($groupAnswers) < 2) {
+            $count += count($groupAnswers[0]);
+        } else {
+            $commonAnswers = count(call_user_func_array('array_intersect', $groupAnswers));
+            $count += $commonAnswers;
+        }
+    }
+
+    return $count;
+}
+
+/**
  * @param $groupAnswers
  * @return int
  */
-function findUnique($groupAnswers)
+function findUnique($groupAnswers): int
 {
     return strlen(count_chars($groupAnswers, 3));
 }
 
 $time_start = microtime(true);
 
-echo countUniqueAnswers($answers);
+echo countUniqueAnswers($answers) . PHP_EOL;
+echo countCommonAnswers($answers) . PHP_EOL;
 
 $time_end = microtime(true);
 $execution_time = ($time_end - $time_start);
-echo 'Total Execution Time: ' . $execution_time .' Seconds';
+echo 'Total Execution Time: ' . $execution_time . ' Seconds';
